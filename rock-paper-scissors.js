69f9f2b75268dca90a276b0621e45578d81d434b
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-22 16:33:49 +0100

    Add game UI and modify script to use it
    Added a basic (unstyled) UI to the webpage index.html; Updated the js
    script to use DOM manipulation based on user interaction with the UI.
    The script now:
     - Let's the user initiate the game and submit their choice via buttons
     - Output the score for each round directly onto the webpage
     - Output the winner of the game directly on the webpage
     - Reset the score and rounds once a game has finished

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,125 +1,129 @@
-// Initiate the game;
-function playGame() {
-// Set player score to 0;
-    let humanScore = 0;
+// Set initial scores and round
+let humanScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
 
-    // Set computer score to 0;
-    let computerScore = 0;
+// Compute rock, paper, scissors choice for the computer;
+function getComputerChoice() {
+    let rps = ["ROCK", "PAPER", "SCISSORS"];
+    let computerRandomChoice = rps[(Math.floor(Math.random() * rps.length))];
+    return computerRandomChoice;
+};
 
-    // Set round counter to 1;
-    let roundCounter = 1;
-
-    // While round counter is less than or equal to 5, play game;
-    while (roundCounter <= 5) {
-        // Prompt user to choose rock, paper, or scissors;
-        function getHumanChoice() {
-            let humanInput = prompt("Type either rock, paper, or scissors: ");
-            // Check if the user input is one of the three options we want, if true return input;
-            if ((humanInput.toUpperCase() === "ROCK") || (humanInput.toUpperCase() === "PAPER") || (humanInput.toUpperCase() === "SCISSORS")) {
-                return humanInput;
-            }
-            // else throw an alert and re-ask for input via recursion;
-            else {
-                alert("Input incorrect, please only type either rock, paper, or scissors");
-                return getHumanChoice();
-            }
+function roundLogic(humanInput, computerInput) {
+    // Compare user choice to computer choice;
+    let roundOutcome;
+    // If user choice is rock then do;
+    if (humanInput.toUpperCase() === "ROCK") {
+        // If computer choice is rock then ouptput "Tie Game";
+        if (computerInput === "ROCK") {
+            roundOutcome = "You Draw";
         }
-
-        // Compute rock, paper, scissors choice for the computer;
-        function getComputerChoice() {
-            let rps = ["ROCK", "PAPER", "SCISSORS"];
-            let computerRandomChoice = rps[(Math.floor(Math.random() * rps.length))];
-            return computerRandomChoice
+        // Else if computer choice is scissors then output "You Win!";
+        else if (computerInput === "SCISSORS") {
+            roundOutcome = "You Win!"
         }
+        // Else output "You Lose";
+        else {
+            roundOutcome = "You Lose";
+        }
+    }
+    // Else if user choice is scissors then do;
+    else if (humanInput.toUpperCase() === "SCISSORS") {
+        // If computer choice is rock then output "You Lose";
+        if (computerInput === "ROCK") {
+            roundOutcome = "You Lose";
+        }
+        // Else if computer choice is scissors then output "Tie Game";
+        else if (computerInput === "SCISSORS") {
+            roundOutcome = "You Draw";
+        }
+        // Else output "You Win!";
+        else {
+            roundOutcome = "You Win!";
+        }
+    }
+    // Else do;
+    else {
+        // If computer choice is rock then output "You Win!";
+        if (computerInput === "ROCK") {
+            roundOutcome = "You Win!";
+        }
+        // Else if computer choice is scissors then output "You Lose";
+        else if (computerInput === "SCISSORS") {
+            roundOutcome = "You Lose";
+        }
+        // Else output "Tie Game";
+        else {
+            roundOutcome = "You Draw";
+        }
+    }
 
-        // Get computer and user choices;
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
+    return roundOutcome;
+}
 
-        // Compare user choice to computer choice;
-        let roundOutcome;
-            // If user choice is rock then do;
-            if (humanChoice.toUpperCase() === "ROCK") {
-                // If computer choice is rock then ouptput "Tie Game";
-                if (computerChoice === "ROCK") {
-                    roundOutcome = "You Draw";
-                }
-                // Else if computer choice is scissors then output "You Win!";
-                else if (computerChoice === "SCISSORS") {
-                    roundOutcome = "You Win!"
-                }
-                // Else output "You Lose";
-                else {
-                    roundOutcome = "You Lose";
-                }
-            }
-            // Else if user choice is scissors then do;
-            else if (humanChoice.toUpperCase() === "SCISSORS") {
-                // If computer choice is rock then output "You Lose";
-                if (computerChoice === "ROCK") {
-                    roundOutcome = "You Lose";
-                }
-                // Else if computer choice is scissors then output "Tie Game";
-                else if (computerChoice === "SCISSORS") {
-                    roundOutcome = "You Draw";
-                }
-                // Else output "You Win!";
-                else {
-                    roundOutcome = "You Win!";
-                }
-            }
-            // Else do;
-            else {
-                // If computer choice is rock then output "You Win!";
-                if (computerChoice === "ROCK") {
-                    roundOutcome = "You Win!";
-                }
-                // Else if computer choice is scissors then output "You Lose";
-                else if (computerChoice === "SCISSORS") {
-                    roundOutcome = "You Lose";
-                }
-                // Else output "Tie Game";
-                else {
-                    roundOutcome = "You Draw";
-                }
-            }
-        
-        // Print the output to the console;
-        console.log("Round " + roundCounter + " Outcome: " + roundOutcome);
-
-        // Incrementally increase winners score;
-            // If winner result is "You Win!" increment score by 1;
-            if (roundOutcome === "You Win!") {
-                humanScore++;
-            }
-            else if (roundOutcome === "You Lose") {
-                computerScore++;
-            }
-            // Else no score change;
-        
-        // Print players scores to console;
-        console.log("User: " + humanScore + " - " + computerScore + " :Computer");
-
-        // Increment round counter by 1;
+function playRound(humanInput) {
+    // check scores to continue or restart the game
+    if (Math.max(humanScore, computerScore) < 5) {
         roundCounter++;
+    }
+    else {
+        humanScore = 0;
+        computerScore = 0;
+        roundCounter = 1;
+    }
 
-        // If round counter less than or equal to 5, print round counter;
-        if (roundCounter <= 5) {
-            console.log("Round " + roundCounter + " start!");
-        }
-        // If round counter is equal to 6 compare player scores
-        // Print overall winner to console
-        else if (roundCounter === 6) {
-            if (humanScore > computerScore) {
-                console.log("You Won the Game!!!");
-            }
-            else if (humanScore < computerScore) {
-                console.log("You Lost the Game :(");
-            }
-            else {
-                console.log("The game was a Draw!");
-            }
-        }
-    // End while loop
+    // Get computer and user choices;
+    let humanChoice = humanInput;
+    let computerChoice = getComputerChoice();
+
+    // who won?
+    let roundOutcome;
+    roundOutcome = roundLogic(humanChoice, computerChoice);
+
+    // score logic
+    if (roundOutcome === "You Win!") {
+        humanScore++;
+    }
+    else if (roundOutcome === "You Lose") {
+        computerScore++;
+    }
+
+    return roundOutcome;
+}
+
+function printScores(humanScore, computerScore, roundCounter, roundOutcome) {
+
+    // We need to set the white-space attribute for the linebreak to function correctly when adding textContent;
+    const paraOne = document.createElement("p");
+    paraOne.setAttribute('style', 'white-space: pre;');
+
+    paraOne.textContent = "Round " + roundCounter + " Outcome: " + roundOutcome + "\r\n";
+    paraOne.textContent += "Player: " + humanScore + " point, Computer: " + computerScore + " point.";
+    
+
+    div.appendChild(paraOne);
+   
+    if (humanScore === 5) {
+        const outcome = document.createElement('h1');
+        outcome.textContent = "CONGRATULATIONS! YOU WIN! YOU BEAT THE COMPUTER!";
+        div.appendChild(outcome); 
+    }
+    else if (computerScore === 5) {
+        const outcome = document.createElement('h1');
+        outcome.textContent = "OH NO!! YOU LOST!! COMPUTER WORLD DOMINATION IMMINENT!!!";
+        div.appendChild(outcome);
     }
 }
+
+
+const btns = document.querySelectorAll("button");
+const div = document.querySelector("#results");
+
+btns.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+        roundOutput = playRound(button.textContent);
+        printScores(humanScore, computerScore, roundCounter, roundOutput);
+    });
+});
